00ca6124bde9f7ae9f8943ed32fa242044cccbaa
promahbubul <mahbublalam500@gmail.com>
2024-09-05 21:43:27 +0600

    Home page [story section] updated

--- src/components/Story/index.jsx
+++ src/components/Story/index.jsx
@@ -1,10 +1,11 @@
 import { storyList } from "../../constant/home.constant";
 import Card from "./Card";
 import storyImage from "../../assets/images/story-iamge.svg";
+import storySignature from "../../assets/images/story-signature.svg";
 
 const Story = () => {
   return (
-    <section className="bg-lightCream py-44 relative -z-10">
+    <section className="bg-lightCream py-44  -z-10 border-4">
       <div className="container mx-auto">
         {/* Story Top */}
         <div className="flex flex-row items-center justify-between gap-10">
@@ -18,26 +19,23 @@ const Story = () => {
           ))}
         </div>
         {/* Story Bottom */}
-        <div className="flex flex-row items-center gap-10  mt-36">
-          <div
-            className="w-full border-2
-           border-red-500 "
-          >
-            <img src={storyImage} alt="" className="" />
+        <div className="flex flex-row   gap-10  mt-36">
+          <div className="w-full">
+            <img src={storyImage} alt="" className="w-full" />
           </div>
           <div
-            className="w-2 bg-[#B29A90]  border-2
+            className="w-[2px] bg-[#B29A90] 
            border-red-500 "
           ></div>
           <div
-            className="w-full border-2 h-20  border-2
-           border-red-500  "
+            className="w-full  h-20  
+            "
           >
             <div className="">
               <h1 className="text-[40px] font-cormorant font-bold">
                 The Story
               </h1>
-              <p className="font-josefin text-xl mt-5">
+              <p className="font-josefin text-xl mt-5 text-[555555]">
                 Lorem Ipsum is that it has a more-or-less normal distribution of
                 letters, as opposed to using 'Content here, content gfshere',
                 makinlook like readable English. Many desktop publishing
@@ -46,27 +44,22 @@ const Story = () => {
             </div>
             <div className="flex flex-row items-start justify-between mt-10 gap-10">
               <div className="">
-                <h1 className="text-[40px] font-cormorant font-bold">
-                  The Story
-                </h1>
+                <h1 className="text-[40px] font-cormorant font-bold">1996</h1>
                 <p className="font-josefin text-xl mt-5">
                   Lorem Ipsum is that it has a more-or-less normal distribution
-                  of letters, as opposed to using 'Content here, content
-                  gfshere', makinlook like readable English. Many desktop
-                  publishing packages.
                 </p>
               </div>
               <div className="">
-                <h1 className="text-[40px] font-cormorant font-bold">
-                  The Story
-                </h1>
+                <h1 className="text-[40px] font-cormorant font-bold">2001</h1>
                 <p className="font-josefin text-xl mt-5">
-                  Lorem Ipsum is that it has a more-or-less normal distribution
-                  of letters, as opposed to using 'Content here, content
-                  gfshere', makinlook like readable English. Many desktop
-                  publishing packages.
+                  Lorem Ipsum is that it has a more-or-less normal Content
+                  content
                 </p>
               </div>
+            </div>
+            <div className="mt-9">
+              <h5 className="text-xl text-dark">JOSEFINE</h5>
+              <img src={storySignature} alt="" className="" />
             </div>
           </div>
         </div>
